0ad7791621afddb352a2bd4e2970fd59fdb0ba3f
Sam Duggan <samueljduggan@gmail.com>
2025-04-19 17:32:50 -0400

    [PROJ] added search and grouping/collapsing posts in sidebar

---
+++ src/Kambaz/Courses/Pazza/Sidebar/PostCategory.tsx
@@ -0,0 +1,38 @@
+import { useParams } from 'react-router';
+import PostItem from './PostItem';
+import { useState } from 'react';
+import { GoTriangleDown, GoTriangleRight } from 'react-icons/go';
+
+export default function PostCategory({
+  posts,
+  category,
+}: {
+  posts: any[];
+  category: string;
+}) {
+  const [showCategory, setShowCategory] = useState(true);
+  const { cid } = useParams();
+  if (category === 'PINNED') {
+    console.log(posts, showCategory);
+  }
+  return (
+    <div className="pinned-section">
+      <div className="d-flex flex-row justify-content-between align-items-center pinned-header">
+        <div>{category}</div>
+        {showCategory ? (
+          <GoTriangleDown onClick={() => setShowCategory(false)} />
+        ) : (
+          <GoTriangleRight onClick={() => setShowCategory(true)} />
+        )}
+      </div>
+      {showCategory &&
+        posts
+          .sort(
+            (postA, postB) =>
+              new Date(postB.createDate).getTime() -
+              new Date(postA.createDate).getTime()
+          )
+          .map((post) => <PostItem key={post._id} post={post} cid={cid} />)}
+    </div>
+  );
+}
--- src/Kambaz/Courses/Pazza/Sidebar/index.tsx
+++ src/Kambaz/Courses/Pazza/Sidebar/index.tsx
@@ -1,53 +1,81 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import '../Pazza.css';
 import { useSelector } from 'react-redux';
-import PostItem from './PostItem';
 import { useEffect, useState } from 'react';
+import PostCategory from './PostCategory';
+import groupPostsByDate from './groupDatesByWeek';
+import { MdOutlinePostAdd } from 'react-icons/md';
 
 export default function Sidebar() {
   const { cid } = useParams();
   const { posts, folderFilter } = useSelector(
     (state: any) => state.postsReducer
   );
+
   const [postsToShow, setPostsToShow] = useState(posts);
+  const [groupedPosts, setGroupedPosts] = useState(
+    groupPostsByDate(postsToShow.filter((post) => !post.isPinned))
+  );
+  const [search, setSearch] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (folderFilter) {
+    if (folderFilter && search) {
+      setPostsToShow(
+        posts.filter((post) => post.folders.includes(folderFilter) && post.title.toLowerCase().includes(search.toLowerCase()))
+      );
+    } else if (folderFilter) {
       setPostsToShow(
         posts.filter((post) => post.folders.includes(folderFilter))
+      );
+    } else if (search) {
+      setPostsToShow(
+        posts.filter((post) => post.title.toLowerCase().includes(search.toLowerCase()))
       );
     } else {
       setPostsToShow(posts);
     }
-  }, [folderFilter]);
+  }, [folderFilter, posts, search]);
+
+  useEffect(() => {
+    setGroupedPosts(
+      groupPostsByDate(postsToShow.filter((post) => !post.isPinned))
+    );
+  }, [postsToShow]);
 
   return (
     <div className="pazza-sidebar">
-      <button
-        className="btn btn-sm btn-primary"
-        onClick={() => navigate(`/Kambaz/Courses/${cid}/Pazza/Create`)}
-      >
-        Create Post
-      </button>
-      {postsToShow.filter((post) => post.isPinned).length > 0 && (
-        <div className="pinned-section">
-          <div className="pinned-header">PINNED</div>
-          {posts
-            .filter((post) => post.isPinned)
-            .map((post) => (
-              <PostItem key={post._id} post={post} cid={cid} />
-            ))}
-        </div>
-      )}
-      <div className="pinned-section">
-        <div className={'pinned-header'}>UNPINNED</div>
-        {postsToShow
-          .filter((post) => !post.isPinned)
-          .map((post) => (
-            <PostItem key={post._id} post={post} cid={cid} />
-          ))}
+      <div className="d-flex flex-row bg-secondary">
+        <button
+          color="#3973a1"
+          className="btn btn-sm m-1 w-50 create-button"
+          onClick={() => navigate(`/Kambaz/Courses/${cid}/Pazza/Create`)}
+        >
+          <div className="d-flex flex-row align-items-center">
+            <MdOutlinePostAdd className="me-1" />
+            New Post
+          </div>
+        </button>
+        <input
+          type="text"
+          className="form-control form-control-sm search-input m-1"
+          placeholder="Search..."
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
       </div>
+      {postsToShow.filter((post) => post.isPinned).length > 0 && (
+        <PostCategory
+          posts={postsToShow.filter((post) => post.isPinned)}
+          category="PINNED"
+        />
+      )}
+      {Object.entries(groupedPosts)
+        .filter(([_, postGroup]) => postGroup.length > 0)
+        .map(
+          ([category, postGroup]) =>
+            postGroup && <PostCategory posts={postGroup} category={category} />
+        )}
     </div>
   );
 }
